1071ce87ea6f69e5e942f578e21adf76e73380e1
Axel Heider <axel.heider@hensoldt.net>
2022-06-16 20:45:18 +0200

    [DEV] quote strings

--- helper.cmake
+++ helper.cmake
@@ -53,17 +53,17 @@ if (SDK_USE_CAMKES)
         "${CMAKE_CURRENT_LIST_DIR}/libs/sel4_global_components"
     )
 
-    find_package(camkes-tool REQUIRED)
+    find_package("camkes-tool" REQUIRED)
     enable_language(C CXX ASM)
     camkes_tool_setup_camkes_build_environment()
 
-    find_package(sel4_projects_libs REQUIRED)
+    find_package("sel4_projects_libs" REQUIRED)
     sel4_projects_libs_import_libraries()
 
     # Provide just the connectors from the global component, but no components,
     # because this may cause name conflicts. Any project that needs them must
     # either cherry-pick things or call global_components_import_project().
-    find_package(global-components REQUIRED)
+    find_package("global-components" REQUIRED)
     include("${GLOBAL_COMPONENTS_DIR}/global-connectors.cmake")
     # define a custom variable to legacy compatibility
     set(SDK_SEL4_CAMKES_GLOBAL_COMPS_DIR "${GLOBAL_COMPONENTS_DIR}")
@@ -71,7 +71,7 @@ if (SDK_USE_CAMKES)
 else()
 
     # Add just the seL4 kernel and basic platform settings
-    find_package(seL4 REQUIRED)
+    find_package("seL4" REQUIRED)
     sel4_configure_platform_settings()
 
     # Leave the rest to the project, e.g.
@@ -79,22 +79,22 @@ else()
     #   sel4_import_kernel()
     #   sel4_import_libsel4()
     #
-    #   find_package(elfloader-tool REQUIRED)
+    #   find_package("elfloader-tool" REQUIRED)
     #   elfloader_import_project()
     #
-    #   find_package(musllibc REQUIRED)
+    #   find_package("musllibc" REQUIRED)
     #   musllibc_setup_build_environment_with_sel4runtime()
     #
-    #   find_package(util_libs REQUIRED)
+    #   find_package("util_libs" REQUIRED)
     #   util_libs_import_libraries()
     #
-    #   find_package(seL4_libs REQUIRED)
+    #   find_package("seL4_libs" REQUIRED)
     #   sel4_libs_import_libraries()
     #
-    #   find_package(projects_libs REQUIRED)
+    #   find_package("projects_libs" REQUIRED)
     #   projects_libs_import_libraries()
     #
-    #   find_package(sel4_projects_libs REQUIRED)
+    #   find_package("sel4_projects_libs" REQUIRED)
     #   sel4_projects_libs_import_libraries()
 
 endif()
